654062488ce69f708db703e05b6a9abf43472492
cdcordobaa <cdcordobaa@unal.edu.co>
2020-05-20 17:41:42 -0500

    modified student, an changed insert elements to the tree

--- NonLinear.ts
+++ NonLinear.ts
@@ -5,6 +5,7 @@ class Student implements IComparable<Student> {
   id: number = undefined;
   name: string = undefined;
   tel: string = undefined;
+  nickName: string = undefined;
 
   constructor(id: number, name: string, tel: string) {
     this.id = id;
@@ -27,19 +28,14 @@ class Student implements IComparable<Student> {
 
 let myAVLInstance = new AVL<Student>();
 
-let student1 = new Student(10, "Super Man", "254");
-let student2 = new Student(20, "Super Man", "254");
-
-let student3 = new Student(60, "Super Man", "254");
-let student4 = new Student(40, "Super Man", "254");
-let student5 = new Student(5, "Super Man", "254");
+let student1 = new Student(20, "Super Man", "254");
+let student2 = new Student(30, "Super Man", "254");
+let student3 = new Student(40, "Super Man", "254");
 
 console.log("st", student1);
 
 myAVLInstance.insert(student1);
 myAVLInstance.insert(student2);
 myAVLInstance.insert(student3);
-// myAVLInstance.insert(student4);
-// myAVLInstance.insert(student5);
 
 myAVLInstance.prettyPrint();
