6bedb10b7e06519efb2f35628ab7c81108dd272a
cdcordobaa <cdcordobaa@unal.edu.co>
2020-05-20 17:47:54 -0500

    added career to the student

--- NonLinear.ts
+++ NonLinear.ts
@@ -5,11 +5,13 @@ class Student implements IComparable<Student> {
   id: number = undefined;
   name: string = undefined;
   tel: string = undefined;
+  career: string;
 
-  constructor(id: number, name: string, tel: string) {
+  constructor(id: number, name: string, tel: string, career: string) {
     this.id = id;
     this.name = name;
     this.tel = tel;
+    this.career = career;
   }
 
   compareTo(b: Student) {
@@ -27,19 +29,21 @@ class Student implements IComparable<Student> {
 
 let myAVLInstance = new AVL<Student>();
 
-let student1 = new Student(10, "Super Man", "254");
-let student2 = new Student(20, "Super Man", "254");
+let student1 = new Student(50, "Super Man", "254");
+let student2 = new Student(5, "Super Man", "254");
 
-let student3 = new Student(60, "Super Man", "254");
-let student4 = new Student(40, "Super Man", "254");
+let student3 = new Student(80, "Super Man", "254");
+let student4 = new Student(1, "Super Man", "254");
 let student5 = new Student(5, "Super Man", "254");
+let student6 = new Student(55, "Super Man", "254");
 
 console.log("st", student1);
 
 myAVLInstance.insert(student1);
 myAVLInstance.insert(student2);
 myAVLInstance.insert(student3);
-// myAVLInstance.insert(student4);
-// myAVLInstance.insert(student5);
+myAVLInstance.insert(student4);
+myAVLInstance.insert(student5);
+myAVLInstance.insert(student6);
 
 myAVLInstance.prettyPrint();
